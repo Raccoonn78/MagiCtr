604670389b0dea463932a41deb58ce054abcfaef
Raccoonn78 <kirabo007@gmail.com>
2023-10-01 19:11:57 +0300

    in laptop

---
+++ Интеллектуальные системы и технологии/pr1/pr1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np 
+import sklearn
+import random
+
+# import apriori
+# df = pd.read_csv('C:\\Users\\Дмитрий\\Desktop\\МАГИСТР\\Интеллектуальные_системы\\pr1\datasets\\archive_beer\\beer_and_diaper.csv')
+
+# print(list(df))
